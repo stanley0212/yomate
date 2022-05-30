a5e9316b617bb5ee1de8ff9c7c72032c656418e9
Stanley <trip7.net@gmail.com>
2022-05-31 09:46:30 +1000

    add multi image picker function

--- lib/screens/campsite_googlemap_screen.dart
+++ lib/screens/campsite_googlemap_screen.dart
@@ -157,7 +157,12 @@ class _CampsiteGoogleMapScreenState extends State<CampsiteGoogleMapScreen> {
                                 SizedBox(
                                   //width: 100,
                                   child: Text(
-                                    element.data()['CamperSiteName'],
+                                    element.data()['CamperSiteName'].length > 30
+                                        ? element
+                                                .data()['CamperSiteName']
+                                                .substring(0, 30) +
+                                            '...'
+                                        : element.data()['CamperSiteName'],
                                     maxLines: 1,
                                     //overflow: TextOverflow.fade,
                                     //softWrap: false,
@@ -176,7 +181,13 @@ class _CampsiteGoogleMapScreenState extends State<CampsiteGoogleMapScreen> {
                             padding: const EdgeInsets.only(
                                 top: 10, left: 10, right: 10),
                             child: Text(
-                              element.data()['CamperSiteDescription'],
+                              element.data()['CamperSiteDescription'].length >
+                                      80
+                                  ? element
+                                          .data()['CamperSiteDescription']
+                                          .substring(0, 80) +
+                                      '...'
+                                  : element.data()['CamperSiteDescription'],
                               style: const TextStyle(color: Colors.black),
                               maxLines: 2,
                             ),
@@ -196,10 +207,8 @@ class _CampsiteGoogleMapScreenState extends State<CampsiteGoogleMapScreen> {
                           //   padding:
                           //       EdgeInsets.only(top: 10, left: 10, right: 10),
                           //   child: Text(
-                          //     'Facility:' +
-                          //         element.data()['CamperSiteSummary'].length,
+                          //     'Facility:' + element.data()['CamperSiteSummary'],
                           //     style: const TextStyle(color: Colors.black),
-                          //     maxLines: 1,
                           //   ),
                           // ),
                           GestureDetector(
