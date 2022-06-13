3375e94aaf9e4df612c344924756d64c9b67764e
Stanley <trip7.net@gmail.com>
2022-06-13 21:02:21 +1000

    fix multi images upload

---
+++ ios/ImageNotification/NotificationService.h
@@ -0,0 +1,12 @@
+//
+//  NotificationService.h
+//  ImageNotification
+//
+//  Created by Stanley on 13/6/2022.
+//
+
+#import <UserNotifications/UserNotifications.h>
+
+@interface NotificationService : UNNotificationServiceExtension
+
+@end
